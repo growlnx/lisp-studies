d321545206d78f3a07f66ee09f3dd8707ef55989
Maycon Douglas <growlnx@protonmail.com>
2019-12-27 18:25:23 -0300

    Construindo CLI Parser

--- io/cli_parser.lisp
+++ io/cli_parser.lisp
@@ -5,9 +5,9 @@
 (defun help	()
 	(format t "~A - version: ~A~%" program version)
 	(format t "Usage: ~A [OPTIONS]~2%OPTIONS:~%" (file-namestring *LOAD-PATHNAME*) )
-	(format t "~5T--h,-help      Show this help mensage~%")
-	(format t "~5T--i FILE,-input FILE     Input file [REQUIRED]~%")
-	(format t "~5T--o FILE,-output FILE    Output file [REQUIRED]~%")
+	(format t "~5T--h, -help                Show this help mensage [OPTIONAL]~%")
+	(format t "~5T--i FILE, -input  FILE    Input file             [REQUIRED]~%")
+	(format t "~5T--o FILE, -output FILE    Output file            [REQUIRED]~%")
 )
 
 ; parsing ...
@@ -23,26 +23,23 @@
       
 	(if (or (string= arg '"--h") (string= arg '"-help"))
 		(quit (help))
-    	; elsif
+
 	    (if (or (string= arg '"--i") (string= arg '"-input"))
 	    	(progn
 	    		(setf arguments (cdr arguments)) ; remove --i
 	    		(setf (gethash 'input *options*) (car arguments)) ; store input
 	    		(setf arguments (cdr arguments)) ; remove input
-	    		; (write (gethash 'input *options*))
 	    	)
-	    	; elsif
-		    (if (or (string= arg '"--o") (string= arg '"-output"))
+	    
+	        (if (or (string= arg '"--o") (string= arg '"-output"))
 		    	(progn
 		    		(setf arguments (cdr arguments)) ; remove --o
 		    		(setf (gethash 'output *options*) (car arguments)) ; store input
 		    		(setf arguments (cdr arguments)) ; remove output
-		    		; (write (gethash 'output *options*))
 		    	)
 
-		    	; else
 		    	(progn
-			   		(format t "~%invalid option ~A~%" arg )
+			   		(format t "Invalid Option `~A`~%" arg )
 			    	(quit)
 			    )
     		)
@@ -53,9 +50,9 @@
 (makunbound 'arguments)
 
 (if (eql (gethash 'input *options*) NIL)
-	(format t "[--i FILE, -input FILE]   Input are required!~%")
+	(format t "[--i FILE, -input FILE]   Input are Required!~%")
 )
 
 (if (eql (gethash 'output *options*) NIL)
-	(format t "[--o FILE, -output FILE]  Output are required!~%")
+	(format t "[--o FILE, -output FILE]  Output are Required!~%")
 )
